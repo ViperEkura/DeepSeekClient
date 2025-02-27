57bbc7f3a8ff6457e0dc66982b4748d7bcd1a497
ViperEkura <3081035982@qq.com>
2025-02-27 21:47:40 +0800

    first commit

---
+++ main.py
@@ -0,0 +1,51 @@
+from openai import OpenAI
+from typing import List, Tuple
+import json
+
+
+class DeepSeekClient:
+    def __init__(self, api_key, base_url, model="deepseek-chat", init_prompt="你是一个人工智能助手"):
+        self.model = model
+        self.init_prompt = init_prompt
+        self.client = OpenAI(api_key=api_key, base_url=base_url)
+
+    def stream_chat(self, user_message: str, histories: List[Tuple]=None):
+        if histories is None:
+            histories = []
+        
+        histories.append({"role": "system", "content": self.init_prompt})
+        histories.append({"role": "user", "content": user_message})
+        response = self.client.chat.completions.create(
+            model=self.model,
+            messages=histories,
+            stream=True,
+        )
+
+        for chunk in response:
+            if chunk.choices[0].delta.content:
+                content = chunk.choices[0].delta.content
+                yield content
+
+        yield "\n"
+        
+def save_histoty(histories, output_path):
+    with open(output_path, "w") as f:
+        json.dump(histories, f)
+        
+def config_loader(config_path="./config.json"):
+    with open(config_path, "r") as f:
+        config = json.load(f)
+        return config["api_key"], config["base_url"]
+
+
+if __name__ == "__main__":
+    api_key, base_url, init_prompt = config_loader()
+    client = DeepSeekClient(api_key=api_key, base_url=base_url, init_prompt=init_prompt)
+    
+    
+    while True:
+        user_input = input(">> ")
+        if user_input.lower() == "!exit":
+            break
+        for chunk in client.stream_chat(user_input):
+            print(chunk, end="", flush=True)
