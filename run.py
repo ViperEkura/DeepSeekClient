99192e8901a658b586d7bb8c39e5960056f796a4
ViperEkura <3081035982@qq.com>
2025-09-14 14:31:59 +0800

    feat(run): 添加运行脚本以启动 DeepSeek 聊天应用

---
+++ run.py
@@ -0,0 +1,14 @@
+from core.backend import DeepSeekChatApp
+import json
+ 
+
+def config_loader(config_path="./config.json"):
+    with open(config_path, "r") as f:
+        config = json.load(f)
+        return config["api_key"], config["base_url"], config["init_prompt"]
+
+
+if __name__ == "__main__":
+    api_key, base_url, init_prompt = config_loader()
+    client = DeepSeekChatApp(api_key, base_url)
+    client.run()
